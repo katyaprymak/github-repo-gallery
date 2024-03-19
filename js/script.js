e41f125ba124589505322995392262124ddcda6f
Katya Prymak <62486148+katyaprymak@users.noreply.github.com>
2024-03-19 16:22:23 +0100

    adding repos in js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //Selecting overview class
 const selectOverview = document.querySelector(".overview");
 const username = 'katyaprymak';
+const unordered = document.querySelector(".repo-list");
 
 //async function to fetch data from my Github Profile
 
@@ -27,4 +28,23 @@ const displayUserInfo = function (data) {
         </div> 
     `;
     selectOverview.append(div);
+    reposInfo();
 }
+
+const reposInfo = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const reposData = await fetchRepos.json();
+    eachRepoInfo(reposData);
+}
+
+const eachRepoInfo = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}<h3>`;
+        unordered.append(repoItem);
+    }
+};
+
+
+
